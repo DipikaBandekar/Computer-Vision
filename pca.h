c037e5ecd4cff9da5b6d55939b8c9b1d3fc68c38
Cyril <cshelke@indiana.edu>
2016-03-24 22:16:44 -0400

    reducing dimensions

--- pca.h
+++ pca.h
@@ -9,23 +9,41 @@ class pca : public Classifier {
          for(Dataset::const_iterator c_iter=filenames.begin(); c_iter != filenames.end(); ++c_iter)
 	     {
 			cout << "Processing " << c_iter->first << endl;
-			CImg<double> class_vectors(size*size*3, c_iter->second.size(), 1);
+			CImg<double> class_vectors(size*size, c_iter->second.size(), 1);
 
 			// convert each image to be a row of this "model" image
 			for(int i=0; i<c_iter->second.size(); i++)
 			  class_vectors = class_vectors.draw_image(0, i, 0, 0, extract_features(c_iter->second[i].c_str()));
 			
-			class_vectors = subtract_mean(class_vectors);
-			CImg<double> mul = covariate(class_vectors);
-			cout<<"rows and cols of matrix mux: "<<mul.width()<<"   "<<mul.height()<<endl;
-			//cout<<"class_vectors.rows() and .cols()"<<class_vectors.height()<<"  "<<class_vectors.width()<<endl;
-			class_vectors.save_png(("pca_model." + c_iter->first + ".png").c_str());
+			class_vectors = subtract_mean(class_vectors); //50 X 1600
+			CImg<double> mul = covariate(class_vectors);  // 1600 X 1600
+			
+			//getting the eigen values
+			CImg<double> eigen(class_vectors.width(),class_vectors.width(),1);
+			mul.symmetric_eigen(eigen,eigen);
+
+			int k = size*size/4;
+			cout<<"k: "<<k<<endl;
+
+			//reducing the eigen matrix from n X n to n X k
+			CImg<double> reducedEigen(k,eigen.height(),1);
+			for(int i=0; i<eigen.height();i++)
+			{
+				for(int j=0; j<k; j++)
+					reducedEigen(j,i) = eigen(j,i);
+			}
+
+			CImg<double> pcaMatrix = class_vectors * reducedEigen;
+
+			cout<<"size of reduced Eigen: "<<pcaMatrix.height()<<" X "<<pcaMatrix.width()<<endl;
+
+			//class_vectors.save_png(("pca_model." + c_iter->first + ".png").c_str());
 	      }		
 	}
 
 	CImg<double> covariate(CImg<double> class_vectors)
 	{
-		CImg<double> multiplied(class_vectors.width(),class_vectors.width(),1)																																																																																																																																																																																																																																																																																																																																																											;
+		CImg<double> multiplied(class_vectors.width(),class_vectors.width(),1);																																																																																																																																																																																																																																																																																																																																																											;
 		for (int i=0; i<class_vectors.height(); i++) 
 		{
 			CImg<double> col_vector(class_vectors.width(),1,1); // cols = n, rows =1
@@ -43,37 +61,37 @@ class pca : public Classifier {
 		return multiplied;
 	}
 
-	CImg<double> subtract_mean(CImg<double> class_vectors)
+	CImg<double> subtract_mean(CImg<double> c_vectors)
 	{
 		//initializing mean array to zero
-		double meanArr[class_vectors.width()] ;
-		for (int i=0;i< class_vectors.width();i++)
+		double meanArr[c_vectors.width()] ;
+		for (int i=0;i< c_vectors.width();i++)
 			meanArr[i] = 0;
 
 		//for addition column wise
-		for (int i=0; i<class_vectors.height(); i++) 
+		for (int i=0; i<c_vectors.height(); i++) 
 		{
-			for (int j=0; j<class_vectors.width(); j++)
+			for (int j=0; j<c_vectors.width(); j++)
 			{
-				meanArr[j] = meanArr[j] + class_vectors(j,i);
+				meanArr[j] = meanArr[j] + c_vectors(j,i);
 			}
 		}
 		
 		//calculating mean for each column
-		int images = class_vectors.height();
-		for (int i=0; i<class_vectors.width(); i++)
+		int images = c_vectors.height();
+		for (int i=0; i<c_vectors.width(); i++)
 			meanArr[i] = meanArr[i]/images;
 
 		//subtracting mean
-		for (int i=0; i<class_vectors.height(); i++) 
+		for (int i=0; i<c_vectors.height(); i++) 
 		{
-			for (int j=0; j<class_vectors.width(); j++)
+			for (int j=0; j<c_vectors.width(); j++)
 			{
-				class_vectors(j,i) = meanArr[j] - class_vectors(j,i);
+				c_vectors(j,i) = meanArr[j] - c_vectors(j,i);
 			}
 		}
 
-		return class_vectors;
+		return c_vectors;
 	}
 
 	virtual void load_model() {
